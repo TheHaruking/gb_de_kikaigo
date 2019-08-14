6ea1a57d9c4d03be8d29979de8ac4c3e9c19cccf
TheHaruking <wta2m-c3@yahoo.co.jp>
2019-08-14 19:01:53 +0900

    .z80 → .asm に変更 (構文強調され、見やすくなるため)

--- make.bat
+++ make.bat
@@ -3,7 +3,7 @@ rem ビルドを行う場合は以下の PATH を各自の環境に合わせてください。
 set PATH=PATH;
 set TITLE="GBdeKIKAIGO"
 
-rgbasm -o obj/Fonts2.o src/Fonts2.z80
-rgbasm -o obj/%TITLE%.o src/%TITLE%.z80
+rgbasm -o obj/Fonts2.o src/Fonts2.asm
+rgbasm -o obj/%TITLE%.o src/%TITLE%.asm
 rgblink -m obj/%TITLE%.map -n %TITLE%.sym -o %TITLE%.gbc obj/%TITLE%.o obj/Fonts2.o
 rgbfix -p0 -v %TITLE%.gbc
